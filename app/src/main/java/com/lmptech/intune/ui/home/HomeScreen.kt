21665fc71fc71cca98c1c47e2a1c4aa80452ddd0
ankit <ap8424856@gmail.com>
2024-07-07 00:53:47 +0530

    modified chat screen

--- app/src/main/java/com/lmptech/intune/ui/home/HomeScreen.kt
+++ app/src/main/java/com/lmptech/intune/ui/home/HomeScreen.kt
@@ -1,27 +1,58 @@
 package com.lmptech.intune.ui.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     activeChatId: String,
     modifier: Modifier = Modifier,
     openDrawer: () -> Unit = {},
 ) {
+
+    val text = remember {
+        mutableStateOf("")
+    }
 
     Box(
         modifier = Modifier
@@ -47,7 +78,37 @@ fun HomeScreen(
                 }
             }
 
-            OutlinedTextField(value = "", onValueChange = {})
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp, vertical = 4.dp)
+            ) {
+
+                BasicTextField(
+                    modifier = Modifier
+                        .height(48.dp)
+                        .weight(1f),
+                    maxLines = 3,
+                    value = text.value, onValueChange = { text.value = it },
+                    textStyle = TextStyle(fontSize = 18.sp),
+                    decorationBox = {
+                        Row(
+                            Modifier
+                                .background(
+                                    color = MaterialTheme.colorScheme.surfaceVariant,
+                                    RoundedCornerShape(45.dp)
+                                ).padding(horizontal = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            it()
+                        }
+                    }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                FilledIconButton(onClick = { /*TODO*/ }, modifier = Modifier.size(48.dp)) {
+                    Icon(imageVector = Icons.AutoMirrored.Default.Send, contentDescription = "")
+                }
+            }
         }
     }
 
